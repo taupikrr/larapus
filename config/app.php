c657840f4c926d669f9c97af3691390d7776aa07
151610209-smkassalaam <151610209@smkassalaam.sch.id>
2017-06-13 16:07:25 +0700

    larapus login

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'Larapus',
 
     /*
     |--------------------------------------------------------------------------
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
 
         //
 
@@ -224,6 +225,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="crw">
+    <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Halo</div>
-                    <div class="panel-body">
-                    Selamat Datang di Larapus
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection                    
+@endsection
